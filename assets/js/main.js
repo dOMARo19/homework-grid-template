79c5fec79f0669579d67d05a15a9aeb827532c62
dOMARo19 <omarcuk954@gmail.com>
2024-12-21 15:47:23 +0100

    header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,12 @@
 
 $(document).ready(function () {
 
-  $('.header-navbar__hamburger').on('click', function () {
+  $('.header-navbar__btn').on('click', function () {
     $(this).toggleClass('active');
     $('.header-navbar__list').toggleClass('active');
   });
 
 });
+
+
+
